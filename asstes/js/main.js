5998dd3562bcd77e53e566cb8d9a49ce1bca94e3
Rey <rey@gmail.com>
2025-08-27 14:44:16 +0700

    add auto typing style

--- asstes/js/main.js
+++ asstes/js/main.js
@@ -78,3 +78,27 @@ const observer = new IntersectionObserver((entries, obs) => {
 }, { threshold: 0.2 });
 
 document.querySelectorAll('.counter').forEach(el => observer.observe(el));
+//---------------- auto typing script --------------------------
+let typeJsText = document.querySelector(".typeJsText");
+let textArray = typeJsText.dataset.typetext.split("");
+let counter = -1;
+
+typeJsText.innerHTML = "";
+
+function typeJs() {
+  if (counter < typeJsText.dataset.typetext.length) {
+    counter++;
+    typeJsText.innerHTML += typeJsText.dataset.typetext.charAt(counter);
+    textArray = typeJsText.dataset.typetext.split("");
+  } else {
+    textArray.pop();
+    typeJsText.innerHTML = textArray.join("");
+    if (textArray.length == 0) {
+      counter = -1;
+    }
+  }
+}
+
+setInterval(() => {
+  typeJs();
+}, 100);
